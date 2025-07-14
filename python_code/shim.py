ebe10b89ce80c32ea226d564191cfa6a70dfa89a
dannysun91 <dannysun@berkeley.edu>
2025-07-13 19:18:10 -0700

    new angle

--- python_code/shim.py
+++ python_code/shim.py
@@ -173,9 +173,9 @@ opt_len   = 5.600                  # Radius of optic (from center to screws)
 rot_ang   = 30.0                      # Rotation angle to orient optic wrt Rocket (degrees)
 # OPTIC ALIGNMENT Properties (degrees)
 # Misalignment angle Amount - Usually within 20 arcminutes
-mis_ang = 1.898/60.0
+mis_ang = 0.736/60.0
 # Off Axis Amount (degrees) (from start of unit circle (x=1,y=0), going around CCW (0-360)) - Shim away from shearing
-off_ang = 260
+off_ang = 178
 # Account for 180 shift from perspective of looking at the payload from the front vs the back
 off_ang = (270.0-off_ang)+90.0
 # Compute horizontal and vertical offsets in cartesian frame
